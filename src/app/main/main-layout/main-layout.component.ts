5619f164b4d9f12260382339f92cc094a66a6006
oksanab91 <oksanab767@gmail.com>
2020-05-22 19:55:35 +0300

    Update css styling & remove redundant component

--- src/app/main/main-layout/main-layout.component.ts
+++ src/app/main/main-layout/main-layout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'home-layout',
+  selector: 'main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss']
 })
